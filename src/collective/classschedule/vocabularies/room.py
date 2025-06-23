6c42192067566eb89a83679828d4348193caf50d
adriana_rv <adriana.rv@gmail.com>
2025-06-23 16:34:08 -0600

    Add translations for fatures room

--- src/collective/classschedule/vocabularies/room.py
+++ src/collective/classschedule/vocabularies/room.py
@@ -1,3 +1,4 @@
+from collective.classschedule import _
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
@@ -8,7 +9,7 @@ def FeaturesVocabularyFactory(context):
     """Vocabulary for indicate features."""
 
     return SimpleVocabulary([
-        SimpleVocabulary.createTerm("chalk_board", "chalk_board", "Chalk board"),
-        SimpleVocabulary.createTerm("white_board", "white_board", "White board"),
-        SimpleVocabulary.createTerm("projector", "projector", "Projector"),
+        SimpleVocabulary.createTerm("chalk_board", "chalk_board", _("Chalk board")),
+        SimpleVocabulary.createTerm("white_board", "white_board", _("White board")),
+        SimpleVocabulary.createTerm("projector", "projector", _("Projector")),
     ])
