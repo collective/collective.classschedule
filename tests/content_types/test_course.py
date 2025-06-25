9a3e15c8a7f490c109591733f3cb4d324391f4ea
Gildardo Bautista García Cano <gil.cano@gmail.com>
2025-06-25 16:58:46 -0600

    add more test

---
+++ tests/content_types/test_course.py
@@ -0,0 +1,32 @@
+from plone.dexterity.utils import resolveDottedName
+from zope.component import createObject
+
+import pytest
+
+
+# @pytest.fixture
+# def container(portal) -> DexterityContent:
+#     return portal
+
+
+@pytest.fixture
+def portal_type() -> str:
+    return "Course"
+
+
+class TestContentType:
+    @pytest.fixture(autouse=True)
+    def _setup(self, portal):
+        self.container = portal
+
+    def test_get_fti(self, get_fti, portal_type):
+        assert get_fti(portal_type) is not None
+
+    def test_create(self, get_fti, portal_type):
+        fti = get_fti(portal_type)
+        factory = fti.factory
+        klass = resolveDottedName(fti.klass)
+        obj = createObject(factory)
+        assert obj is not None
+        assert isinstance(obj, klass)
+        assert obj.portal_type == portal_type
